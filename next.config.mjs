2f455b385c2b5aa44ede0d66b4558685fc65ff8f
kp1153 <kamta_tiwari@yahoo.com>
2026-04-11 18:40:58 +0530

    force deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  experimental: {
+    turbo: {
+      enabled: false,
+    },
+  },
 };
 
 export default nextConfig;
